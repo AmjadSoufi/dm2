dfe286b7d5d624ce0e57498c2d88ddaf0524fed3
AmjadSoufi <113971567+AmjadSoufi@users.noreply.github.com>
2025-02-13 10:02:56 +0100

    Update changes in js branch

---
+++ aside.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const closeButton = document.querySelector("aside button");
+    const aside = document.querySelector("aside");
+  
+    closeButton.addEventListener("click", function () {
+      aside.style.display = "none";
+    });
+  });
+  
